a0bcd4775429b52fe4ac173eb66b97282a7ba14b
paul hoover <paulhhoover@hotmail.com>
2013-04-12 21:04:53 -0400

    finally got door animations working by selecting legacy for animation type in rig tab of bathroom model

--- Assets/scripts/BathroomDoor.cs
+++ Assets/scripts/BathroomDoor.cs
@@ -24,13 +24,15 @@ public class BathroomDoor : MonoBehaviour {
 	public void OnMouseDown() {
 		//Debug.Log ( "Mouse down on " + this.name );
 		//gameObject.animation.Play( "door_open" );	
-		Debug.Log( "bathroom = " + bathroom );
+		Debug.Log( "bathroom = " + bathroom.transform.animation.GetClipCount() + ", name = " + bathroom.transform.animation.name );
+		string names = GetAnimationNames (bathroom.transform.animation);
+		//Debug.Log("animations = " + names );
 
 		if( isDoorOpen ) {
             bathroom.transform.animation.Play("close_door");
 			isDoorOpen = false;			
 		} else {
-            bathroom.transform.animation.Play("open_door");
+          bathroom.transform.animation.Play("open_door");
 			isDoorOpen = true;
 		}
 
@@ -39,4 +41,32 @@ public class BathroomDoor : MonoBehaviour {
 	public void OnTriggerEnter(Collider collider) {
 		Debug.Log ( "Door triggered" );
 	}
+	
+	private string GetAnimationNames(Animation anim) {
+		//string[] names;
+		string names = "";
+		int num = 0;
+		foreach(AnimationState state in anim) {
+			num++;
+			Debug.Log(num);
+			names += ", " + state.name;
+		}	
+//		names += ", total = " + num;
+		return names;
+	}
+/*	
+function GetAnimationNames(Animation anim) {
+	// make an Array that can grow
+	var tmpList : Array = new Array();
+ 
+	// enumerate all states
+	for (var state : AnimationState in anim) {
+		// add name to tmpList
+		tmpList.Add(state.name);
+	}
+	// convert to (faster) buildin array (but can't grow anymore)
+	var list : string[] = tmpList.ToBuildin(string);
+	return list;
+}
+*/
 }
