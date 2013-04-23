df757ce52a2774217697c3f6d7b6d0397b9b0222
Paul Hoover <paul.hoover@selectablemedia.com>
2013-04-23 09:01:55 -0400

    flashlight added

---
+++ Assets/scripts/Flashlight.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public bool collected = true;
+
+	bool isOn = false;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
