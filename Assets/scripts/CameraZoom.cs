f5df3fab5e0af512f713f57802015d50464f3b60
Paul Hoover <paul.hoover@selectablemedia.com>
2013-04-24 17:46:44 -0400

    CameraZoom script added; endtable01 positioned and textures looking decent; flashlight cookie added

---
+++ Assets/scripts/CameraZoom.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+	
+	Camera camera;
+	int zoom = 20;
+	int normal = 60;
+	float smooth = 5;
+	bool isZoomed = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("z")) {
+			isZoomed = !isZoomed;
+		}
+		if(isZoomed) {
+			camera.fieldOfView = Mathf.Lerp (camera.fieldOfView,zoom,Time.deltaTime*smooth);
+		} else {
+			camera.fieldOfView = Mathf.Lerp (camera.fieldOfView,normal,Time.deltaTime*smooth);
+		}
+	}
+}
