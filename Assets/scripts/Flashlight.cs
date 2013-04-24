29849fa580510c670c678092875968f8c75c9f0e
Paul Hoover <paul.hoover@selectablemedia.com>
2013-04-24 18:33:30 -0400

    flashlight on/off and zoom working

--- Assets/scripts/Flashlight.cs
+++ Assets/scripts/Flashlight.cs
@@ -6,25 +6,30 @@ public class Flashlight : MonoBehaviour {
 	public bool collected = true;
 
 	bool isOn = false;
-	Light bulb;
 	
 	void Awake() {
 		//bulb = this.transform.GetComp("flashlight_bulb");
-		Debug.Log("bulb = " + bulb);
+		this.light.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(collected) {
 			if(Input.GetKeyDown("f")) {
+				this.light.enabled = !this.light.enabled;
+				/*
 				if(isOn) {
 					// turn off
 					Debug.Log("turning flashlight off");					
+					this.enabled = false;
+					isOn = false;
 				} else {
 					// turn on
 					Debug.Log("turning flashlight on");
+					this.enabled = true;
+					isOn = true;
 				}
-				isOn = !isOn;
+				*/
 			}
 		}
 	}
