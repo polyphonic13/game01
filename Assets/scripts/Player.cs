2cae3f7c8304798724c18b4a88dcd820ca4056c3
paul hoover <paulhhoover@hotmail.com>
2013-05-02 20:12:55 -0400

    disabled mouse looking and character movement while inventory open

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,32 +4,35 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	
 	bool inventoryOpen = false;
+	Camera camera;
 	//InventoryManager inventory;
 	
 	// Use this for initialization
-	void Start () {}
+	void Start () 
+	{
+		camera = Camera.main;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown("q")) {
 			inventoryOpen = !inventoryOpen;
+			disablePlayer(!inventoryOpen);
 		}
 	}
 	
 	void OnGUI() {
 		if(inventoryOpen) {
 			GUI.Box(new Rect(0, 0, Screen.width/2, Screen.height/2), "HELLO INVENTORY!");
-			disablePlayer(true);
-		} else {
-			var mouseLook = GetComponent<MouseLook>();
-			disablePlayer(false);
 		}
 	}
 	
-	void disablePlayer(bool disabled) {
-		var mouseLook = GetHashCode<MouseLook>();
-		mouseLook.isEnabled = disabled;
-		//var character = GetComponent<CharacterMotor>();
-		//character.SetControllable(!disabled);
+	void disablePlayer(bool disable) {
+		var mouseLook = GetComponent<MouseLook>();
+		mouseLook.isEnabled = disable;
+		var cameraMouseLook = camera.GetComponent<MouseLook>();
+		cameraMouseLook.isEnabled = disable;
+		var character = GetComponent<CharacterMotor>();
+		character.SetControllable(disable);
 	}
 }
