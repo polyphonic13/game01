7c3bcba3ad3b7b41e8a5a577c9f8f7dd6c398011
paul hoover <paulhhoover@hotmail.com>
2013-09-15 14:50:46 -0400

    door_plain02: added with armature animation and use of ArmatureParent & ArmatureTrigger

---
+++ Assets/scripts/ArmatureTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArmatureTrigger : MonoBehaviour {
+	public int interactDistance = 3;
+	public ArmatureParent pops;
+	public string openClipName;
+	public string closeClipName;
+	public bool isOpen = false;
+	
+	public void OnMouseDown() {
+		handleAnimation();
+	}
+	
+	public void handleAnimation() {
+		Debug.Log("ArmatureTrigger/OnMouseDown, name = " + this.name);
+		var difference = Vector3.Distance(Camera.mainCamera.gameObject.transform.position, this.transform.position);
+		if(difference <= interactDistance) {
+			if(isOpen) {
+				pops.PlayAnimation(closeClipName);
+			} else {
+				pops.PlayAnimation(openClipName);
+			}
+			isOpen = !isOpen;
+			// if(pops != null) {
+				/*
+				if(pops.currentOpen == this) {
+					pops.CloseChild(this);
+					isOpen = false;
+				} else {
+					pops.OpenChild(this);
+					isOpen = true;
+				}
+				*/
+			// }
+		}
+	}
+}
