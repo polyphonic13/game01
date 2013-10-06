01b4597d2426974b9c1e16a54b5af2a72b7eb6ed
paul hoover <paulhhoover@hotmail.com>
2013-10-05 20:27:30 -0400

    ArmatureParent: animation layering

--- Assets/scripts/armatures/ArmatureParent.cs
+++ Assets/scripts/armatures/ArmatureParent.cs
@@ -8,14 +8,17 @@ public class ArmatureParent : MonoBehaviour {
 	private Animation _animation;
 	
 	public void playAnimation(string clip) {
+		_animation [clip].layer = 10;
+		_animation [clip].blendMode = AnimationBlendMode.Blend;
 		_animation[clip].wrapMode = WrapMode.Once;
 		_animation.Play(clip);
 	}
 	
 	void Start() {
 		_animation = GetComponent<Animation>();
-		//Debug.Log("ArmatureParent/Start, defaultAnimation = " + defaultAnimation.name);
+		Debug.Log("ArmatureParent/Start, defaultAnimation = " + defaultAnimation.name);
 		if(defaultAnimation != null) {
+			_animation [defaultAnimation.name].layer = 0;
 			_animation[defaultAnimation.name].wrapMode = WrapMode.Once;
 			_animation.Play(defaultAnimation.name);
 		}
