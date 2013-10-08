d3baec36e8a583d29f1e4725ee6f942195a1e8c0
Paul Hoover <paul.hoover@selectablemedia.com>
2013-10-08 17:54:48 -0400

    RoomItem.cs added

---
+++ Assets/scripts/RoomItem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomItem : MonoBehaviour {
+	public InteractiveElement ieScript;
+
+	public string containingRoom; 
+	public bool roomActive = false; 
+
+	void Awake() {
+				init ();
+		}
+
+	public void init() {
+		ieScript = gameObject.GetComponent<InteractiveElement> ();
+
+		var eventCenter = EventCenter.Instance;
+		eventCenter.roomWasEntered += this.roomEntered;
+		eventCenter.roomWasLeft += this.roomLeft;
+		}
+
+	public void roomEntered(string room) {
+		if (room == this.containingRoom) {
+						if (ieScript != null) {
+								ieScript.enabled = true;
+						}
+			Debug.Log ("RoomItem[ " + this.name + " ]/roomEntered");
+			roomActive = true;
+		}
+	}
+
+	public void roomLeft(string room) {
+		if (room == this.containingRoom) {
+			Debug.Log ("RoomItem[ " + this.name + " ]/roomLeft");
+						if (ieScript != null) {
+								ieScript.enabled = false;
+						}
+			roomActive = false;
+		}
+	}
+
+}
