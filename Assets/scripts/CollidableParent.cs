5178f7f30568bee00ede173ac4cce795ad2aa019
Paul Hoover <paul.hoover@selectablemedia.com>
2013-10-16 19:45:46 -0400

    ItemContainer added to extend CollidableParent (collisionObject moved)

--- Assets/scripts/CollidableParent.cs
+++ Assets/scripts/CollidableParent.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class CollidableParent : MonoBehaviour {
 	
-	private GameObject _containerSpot;
-	
 	// Use this for initialization
 	void Awake () {
 		init ();	
@@ -18,45 +16,34 @@ public class CollidableParent : MonoBehaviour {
 		var childTransforms = gameObject.GetComponentsInChildren<Transform>();
 		
 		foreach(Transform childTransform in childTransforms) {
+			Debug.Log("Adding CollidableChild to " + childTransform.gameObject.name);
 			CollidableChild touchableChild = childTransform.gameObject.AddComponent<CollidableChild>();
 			touchableChild.onChildCollision += this.onChildCollision;
-
-			if(childTransform.gameObject.name == "containerSpot") {
-//				Debug.Log("   FOUND CONTAINER SPOT: " + childTransform.gameObject.name);
-				_containerSpot = childTransform.gameObject;
-			}
 		}
 	}
 	
 	public void onChildCollision(GameObject collisionTarget) {
-		_onCollision(collisionTarget);
+		onCollision(collisionTarget);
 	}
 	
-	private void _onCollision(GameObject target) {
-//		Debug.Log("CollidableParent[ " + this.name + " ]/_onCollision, collisionTarget = " + target.name);
+	public void onCollision(GameObject target) {
+		Debug.Log("CollidableParent[ " + this.name + " ]/_onCollision, collisionTarget = " + target.name);
 		if(target.name == "weight(Clone)") {
-//			Debug.Log(" it is a weight");
+			Debug.Log(" it is a weight");
 			ItemWeight itemWeight = target.GetComponent<ItemWeight>();
-//			Debug.Log("  itemWeight = " + itemWeight + ", targetContainerName = " + itemWeight.targetContainerName);
+			Debug.Log("  itemWeight = " + itemWeight + ", targetContainerName = " + itemWeight.targetContainerName);
 			if(itemWeight.targetContainerName != null && itemWeight.targetContainerName == this.name) {
 				Debug.Log("  itemWeight.parent = " + itemWeight.parentObject);
-				_positionContainerChild(itemWeight.parentObject);
+				positionChild(itemWeight.parentObject);
 			}
 		}
 	}
 	
 	void OnCollisionEnter(Collision target) {
-		_onCollision(target.transform.gameObject);
+		onCollision(target.transform.gameObject);
 	}
 	
-	private void _positionContainerChild(GameObject child) {
-		child.transform.parent = _containerSpot.transform;
-		child.transform.position = _containerSpot.transform.position;
-		child.transform.rotation = _containerSpot.transform.rotation; 
-		
-		ItemWeight itemWeight = child.GetComponent<ItemWeight>();
-		if(itemWeight != null) {
-			itemWeight.killSelf();
-		}
+	public virtual void positionChild(GameObject child) {
+		// implement
 	}
 }
---
+++ Assets/scripts/ItemContainer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemContainer : CollidableParent {
+
+	private GameObject _containerSpot;
+	
+	// Use this for initialization
+	void Awake() {
+		init ();
+		_containerSpot = this.transform.Search("containerSpot").gameObject;
+		Debug.Log("ItemContainer/Awake, _containerSpot = " + _containerSpot);
+	}
+	
+	// Update is called once per frame
+	public override void positionChild(GameObject child) {
+		Debug.Log("ItemContainer/positionChild, child = " + child.name + ", _containerSpot = " + _containerSpot.name);
+		child.transform.parent = _containerSpot.transform;
+		child.transform.position = _containerSpot.transform.position;
+		child.transform.rotation = _containerSpot.transform.rotation; 
+		
+		ItemWeight itemWeight = child.GetComponent<ItemWeight>();
+		if(itemWeight != null) {
+			itemWeight.killSelf();
+		}
+	}
+}
