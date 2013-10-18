3c20b22d24d7abb4164c7608196b88acd5003f63
paul hoover <paulhhoover@hotmail.com>
2013-10-18 01:03:03 -0400

    EventAnimationTrigger

--- Assets/scripts/armatures/EventAnimationParent.cs
+++ Assets/scripts/armatures/EventAnimationParent.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class EventAnimationParent : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public AnimationClip animationClip;
+	public string eventName = "";
+	
+	// Use this for initialization
+	void Awake() {
+		if(eventName != "") {
+			EventCenter.Instance.onTriggerEvent += this.onTriggerEvent;
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void onTriggerEvent(string evt) {
+		if(evt == eventName && animationClip != null) {
+			_animation[animationClip.name].wrapMode = WrapMode.Once;
+			_animation.Play(animationClip.name);
+		}
 	}
 }
