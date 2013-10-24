f3a587802695eae740f5f386c55fcebe4fab2cab
Paul Hoover <paul.hoover@selectablemedia.com>
2013-10-24 13:26:36 -0400

    debugging OnOffLight (in bedroom_sw)

--- Assets/scripts/OnOffLight.cs
+++ Assets/scripts/OnOffLight.cs
@@ -3,23 +3,34 @@ using System.Collections;
 
 public class OnOffLight : InteractiveElement {
 
-	Light bulb; 
+	private Light _bulb; 
 
 	void Awake() {
 		init(MouseManager.Instance.INTERACT_CURSOR);
-		bulb = this.transform.Search("light_bulb").light;
-		bulb.enabled = false;
-//		Debug.Log("bulb = " + bulb);
+		_bulb = this.transform.Search("light_bulb").light;
+		_bulb.enabled = false;
+		Debug.Log("OnOffLight[ " + this.name + " ]/Awake, _bulb = " + _bulb);
+	}
+
+	public override void OnMouseOver() {
+		Debug.Log("OnOffLight[ " + this.name + " ]/OnMouseOver, isRoomActive = " + this.isRoomActive + ", isEnabled = " + this.isEnabled);
+		if(this.isRoomActive && this.isEnabled) {
+			mouseOver();
+		}
 	}
 
 	public void OnMouseDown() {
 		Debug.Log("OnOffLight[ " + this.name + " ]/OnMouseDown, isRoomActive = " + this.isRoomActive);
 		if(this.isRoomActive) {
 			var difference = Vector3.Distance(Camera.mainCamera.gameObject.transform.position, this.transform.position);
-			Debug.Log("  difference = " + difference + ", bulb.enabled = " + bulb.enabled);
+			Debug.Log("  difference = " + difference + ", _bulb.enabled = " + _bulb.enabled);
 			if(difference < interactDistance) {
-				bulb.enabled = !bulb.enabled;	
+				_bulb.enabled = !_bulb.enabled;	
 			}
 		}
+	}
+	
+	public void toggle() {
+		_bulb.enabled = !_bulb.enabled;
 	}
 }
