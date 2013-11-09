954a8985bdee88d8f599396f76e036b4614564b8
paul hoover <paulhhoover@hotmail.com>
2013-11-09 16:39:26 -0500

    OnOffSelfIllum and LightFixture scripts added and implemented on bedroom_se chandelier

---
+++ Assets/scripts/LightFixture.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFixture : OnOffLight {
+	
+	private Component[] _onOffSelfIllums;
+	
+	void Awake() {
+		initLamp();
+	}
+
+	void initLamp() {
+		_onOffSelfIllums = GetComponentsInChildren<OnOffSelfIllum>();
+		if(_onOffSelfIllums != null) {
+			_toggleSelfIllums(true);
+		}
+		initOnOffLight();
+	}
+	
+	public override void toggle() {
+		this.toggleBulb();
+		if(_onOffSelfIllums != null) {
+			if(this.getIsOn()) {
+				_toggleSelfIllums(false);
+			} else {
+				_toggleSelfIllums(true);
+			}
+		}
+	}
+	
+	private void _toggleSelfIllums(bool turnOff) {
+		//for(int i = 0; i < _onOffSelfIllums.Count; i++) {
+		foreach(OnOffSelfIllum onOffSelfIllum in _onOffSelfIllums) {
+			if(turnOff) {
+				_turnOffSelfIllum(onOffSelfIllum);
+			} else {
+				_turnOnSelfIllum(onOffSelfIllum);
+			}
+		}
+	}
+	
+	private void _turnOffSelfIllum(OnOffSelfIllum onOffSelfIllum) {
+		onOffSelfIllum.gameObject.transform.renderer.material = onOffSelfIllum.offMaterial;
+	}
+
+	private void _turnOnSelfIllum(OnOffSelfIllum onOffSelfIllum) {
+		onOffSelfIllum.gameObject.transform.renderer.material = onOffSelfIllum.onMaterial;
+	}
+}
