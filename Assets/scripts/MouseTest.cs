274e9e968b179b483105f0458e6b8969382cc6b1
Paul Hoover <paul.hoover@selectablemedia.com>
2014-09-30 11:59:46 -0400

    hooded_player2.blend added (version of keke02_actions); minor project cleanup/rearranging

---
+++ Assets/scripts/MouseTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("mouse test start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseDown() {
+				Debug.Log ("mouse test on mouse down");
+		}
+
+	public void OnMouseOver() {
+				Debug.Log ("mouse test on mouse over");
+		}
+}
