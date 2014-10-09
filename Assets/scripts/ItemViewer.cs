2052a3c74301102652b50fcb6de85a4b57891ea1
Paul Hoover <paul.hoover@selectablemedia.com>
2014-10-09 12:06:30 -0400

    ItemViewer: beginning to implement dynamic target add/remove methods; added inventory icon images to assets

--- Assets/scripts/ItemViewer.cs
+++ Assets/scripts/ItemViewer.cs
@@ -13,10 +13,20 @@ public class ItemViewer : MonoBehaviour {
 	public float zoomSpeed = 0.2f; 
 
 	private Vector3 _startPos;
-	private bool _left = false;
-	private bool _right = false; 
-	private bool _up = false; 
-	private bool _down = false; 
+
+	private GameObject _target;
+
+	public void addItem(GameObject target) {
+		_target = target;
+		center = target.transform;
+	}
+
+	public void removeItem() {
+		if(_target != null) {
+			Destroy(_target);
+			center = null;
+		}
+	}
 
 	void Start () {
 		_startPos = transform.position;
@@ -24,43 +34,45 @@ public class ItemViewer : MonoBehaviour {
 	}
 	
 	void Update () {
+		if(center != null) {
 
-		if(Input.GetKeyDown(KeyCode.R)) {
-			// reset rotation and position
-			transform.rotation = new Quaternion(0,0,0,0);
-			transform.position = _startPos;
-		} else {
-			int orbitX = 0;
-			int orbitY = 0;
-			int move = 0;
+			if(Input.GetKeyDown(KeyCode.R)) {
+				// reset rotation and position
+				transform.rotation = new Quaternion(0,0,0,0);
+				transform.position = _startPos;
+			} else {
+				int orbitX = 0;
+				int orbitY = 0;
+				int move = 0;
 
-			// rotation
-			if(Input.GetKey(KeyCode.A)) {
-				orbitY = 1;
-			} else if(Input.GetKey(KeyCode.D)) {
-				orbitY = -1;
-			}
-			
-			if(Input.GetKey(KeyCode.W)) {
-				orbitX = 1;
-			} else if(Input.GetKey(KeyCode.S)) {
-				orbitX = -1;
-			}
-			
-			_orbit(new Vector3(orbitX, orbitY, 0));
+				// ROTATION
+				// up/down orbit (y axis)
+				if(Input.GetKey(KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+					orbitY = 1;
+				} else if(Input.GetKey(KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+					orbitY = -1;
+				}
+				// left/right orbit (x axis)
+				if(Input.GetKey(KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+					orbitX = 1;
+				} else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+					orbitX = -1;
+				}
+				
+				_orbit(new Vector3(orbitX, orbitY, 0));
 
-			// position
-			if(Input.GetKey (KeyCode.Z)) {
-				move = 1;
-			} else if(Input.GetKey (KeyCode.X)) {
-				move = -1;
-			}
+				// POSITION
+				if(Input.GetKey (KeyCode.Z)) {
+					move = 1;
+				} else if(Input.GetKey (KeyCode.X)) {
+					move = -1;
+				}
 
-			if(move != 0) {
-				_move (move);
+				if(move != 0) {
+					_move (move);
+				}
 			}
 		}
-
 	}
 
 	private void _orbit(Vector3 axis) {
