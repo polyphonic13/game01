823054a12b030006fe3f3f103e137f4f659b19a9
Paul Hoover <paul.hoover@selectablemedia.com>
2014-10-09 10:32:23 -0400

    ItemViewer: implemented z/x zooming

--- Assets/scripts/ItemViewer.cs
+++ Assets/scripts/ItemViewer.cs
@@ -10,6 +10,7 @@ public class ItemViewer : MonoBehaviour {
 	public float radius = 2.0f;
 	public float radiusSpeed = 0.5f;
 	public float rotationSpeed = 80.0f;
+	public float zoomSpeed = 0.2f; 
 
 	private Vector3 _startPos;
 	private bool _left = false;
@@ -23,30 +24,41 @@ public class ItemViewer : MonoBehaviour {
 	}
 	
 	void Update () {
+
 		if(Input.GetKeyDown(KeyCode.R)) {
+			// reset rotation and position
 			transform.rotation = new Quaternion(0,0,0,0);
 			transform.position = _startPos;
 		} else {
-			int x;
-			int y;
-			
+			int orbitX = 0;
+			int orbitY = 0;
+			int move = 0;
+
+			// rotation
 			if(Input.GetKey(KeyCode.A)) {
-				y = 1;
+				orbitY = 1;
 			} else if(Input.GetKey(KeyCode.D)) {
-				y = -1;
-			} else {
-				y = 0;
+				orbitY = -1;
 			}
 			
 			if(Input.GetKey(KeyCode.W)) {
-				x = 1;
+				orbitX = 1;
 			} else if(Input.GetKey(KeyCode.S)) {
-				x = -1;
-			} else {
-				x = 0;
+				orbitX = -1;
 			}
 			
-			_orbit(new Vector3(x, y, 0));
+			_orbit(new Vector3(orbitX, orbitY, 0));
+
+			// position
+			if(Input.GetKey (KeyCode.Z)) {
+				move = 1;
+			} else if(Input.GetKey (KeyCode.X)) {
+				move = -1;
+			}
+
+			if(move != 0) {
+				_move (move);
+			}
 		}
 
 	}
@@ -55,5 +67,11 @@ public class ItemViewer : MonoBehaviour {
 		transform.RotateAround (center.position, axis, rotationSpeed * Time.deltaTime);
 //		desiredPosition = (transform.position - center.position).normalized * radius + center.position;
 //		transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+	}
+
+	private void _move(int forwardFactor) {
+//		transform.position = Vector3.MoveTowards();
+		float speed = zoomSpeed * forwardFactor;
+		this.transform.position += this.transform.forward * speed * Time.deltaTime;
 	}
 }
