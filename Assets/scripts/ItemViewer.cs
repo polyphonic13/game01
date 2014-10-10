35296c66924bf7fe38606d132636b703cdce7ea4
Paul Hoover <paul.hoover@selectablemedia.com>
2014-10-10 10:20:55 -0400

    itemviewer: moving camera instead of entire prefab. dog and rabbit prefab center points normalized; zoom and speed tweaks

--- Assets/scripts/ItemViewer.cs
+++ Assets/scripts/ItemViewer.cs
@@ -5,13 +5,10 @@ public class ItemViewer : MonoBehaviour {
 
 	public Camera viewer;
 	public Transform tgt;
-//	public Vector3 axis = Vector3.up;
-	public float radius = 2.0f;
-	public float radiusSpeed = 0.5f;
-	public float rotationSpeed = 80.0f;
-	public float zoomSpeed = 0.5f; 
-	public float zoomMax = 2f;
-	public float zoomMin = 0.5f;
+	public float rotationSpeed = 150.0f;
+	public float zoomSpeed = 3f; 
+	public float zoomMax = 4.0f;
+	public float zoomMin = 0.05f;
 
 	public bool hasItem { get; set; }
 
@@ -26,7 +23,6 @@ public class ItemViewer : MonoBehaviour {
 		}
 		_target = target;
 		tgt = target.transform;
-		Debug.Log("ItemViewer, new tgt.position = " + tgt.position);
 		hasItem = true;
 	}
 
@@ -34,18 +30,19 @@ public class ItemViewer : MonoBehaviour {
 		if(hasItem) {
 			Destroy(_target);
 			tgt = null;
-			transform.rotation = _startRot;
-			transform.position = _startPos;
+			reset ();
 			hasItem = false;
 		}
 	}
 
+	public void reset() {
+		viewer.transform.rotation = _startRot;
+		viewer.transform.position = _startPos;
+	}
+	
 	void Start () {
-		_startRot = transform.rotation;
-		_startPos = transform.position;
-		Debug.Log("tgt bounds = " + tgt.renderer.bounds.size.z + " tgt = " + tgt.renderer.bounds.center.z);
-		float maxFront = tgt.renderer.bounds.center.z + (tgt.renderer.bounds.size.z/2);
-		Debug.Log("maxFront = " + maxFront);
+		_startRot = viewer.transform.rotation;
+		_startPos = viewer.transform.position;
 	}
 	
 	void Update () {
@@ -53,14 +50,12 @@ public class ItemViewer : MonoBehaviour {
 
 			if(Input.GetKeyDown(KeyCode.R)) {
 				// reset rotation and position
-				transform.rotation = _startRot;
-				transform.position = _startPos;
+				reset ();
 			} else {
 				int orbitX = 0;
 				int orbitY = 0;
 				int move = 0;
-				float dist = (transform.position.z - tgt.position.z);
-				if(dist < 0) { dist = -dist; }
+				var distance = Vector3.Distance(viewer.transform.position, tgt.transform.position);
 
 				// ROTATION
 				// up/down orbit (y axis)
@@ -77,15 +72,15 @@ public class ItemViewer : MonoBehaviour {
 				}
 				
 				_orbit(new Vector3(orbitX, orbitY, 0));
-				Debug.Log("dist = " + dist + ", zoonMin = " + zoomMin + ", zoomMax = " + zoomMax);
+				Debug.Log("distance = " + distance + ", zoonMin = " + zoomMin + ", zoomMax = " + zoomMax);
 				// POSITION
 				if(Input.GetKey (KeyCode.Z) || Input.GetKey(KeyCode.Plus)) {
-					if(dist > zoomMin) {
-						move = -1;
+					if(distance > zoomMin) {
+						move = 1;
 					}
 				} else if(Input.GetKey (KeyCode.X) || Input.GetKey (KeyCode.Minus)) {
-					if(dist < zoomMax) {
-						move = 1;
+					if(distance < zoomMax) {
+						move = -1;
 					}
 				}
 
@@ -97,11 +92,11 @@ public class ItemViewer : MonoBehaviour {
 	}
 
 	private void _orbit(Vector3 axis) {
-		transform.RotateAround (tgt.position, axis, rotationSpeed * Time.deltaTime);
+		viewer.transform.RotateAround (tgt.position, axis, rotationSpeed * Time.deltaTime);
 	}
 
 	private void _move(int forwardFactor) {
 		float speed = zoomSpeed * forwardFactor;
-		transform.position += this.transform.forward * speed * Time.deltaTime;
+		viewer.transform.position += viewer.transform.forward * speed * Time.deltaTime;
 	}
 }
